728246765ad9ba50e750db603f1014c57505e16c
borisgodbout <33127994+borisgodbout@users.noreply.github.com>
2017-11-17 16:47:25 -0500

    Create cardDetector.cpp

---
+++ cardDetector.cpp
@@ -0,0 +1,22 @@
+#include <libarmus.h>
+#include "cardDetector.h"
+
+
+int CardDetector::getCouleur()
+{
+
+		m_cap.color_Read(m_red,m_green,m_blue,m_clear);
+
+
+		LCD_ClearAndPrint("R:%d  G:%d  B:%d  C:%d",m_red,m_green,m_blue,m_clear);
+
+
+	return 0;
+}
+
+int CardDetector::getPoint()
+{
+
+
+	return getCouleur();
+}
